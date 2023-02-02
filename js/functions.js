bcf1424fa3b3f5b04f6f82329c91531d4f136c2c
alrodriguezdg <alrodriguezdg@gmail.com>
2023-02-01 19:36:53 -0500

    layout html and css integration + bootstrap

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+/*DATEPICKER*/
+
+$(function(){
+    $('#datepicker').datepicker();
+  });    
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
