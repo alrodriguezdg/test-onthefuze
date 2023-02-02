c05d141dbf330443e4c31ba066003aa4f2c4eb84
alrodriguezdg <alrodriguezdg@gmail.com>
2023-02-02 06:56:20 -0500

    Fix and optimize UX

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,120 @@
+/* ANIMATIONS */
+gsap.registerPlugin(ScrollTrigger);
+
+const titles = Array.from(document.querySelectorAll("h1, h2, h3, h4, h5"));
+const paragraphs = Array.from(document.querySelectorAll("p"));
+const medias = Array.from(document.querySelectorAll(".animate"));
+const icons = Array.from(document.querySelectorAll(".icon-st"));
+const buttons = Array.from(document.querySelectorAll(".btn"));
+const cards = Array.from(document.querySelectorAll(".card"));
+
+var btn = document.getElementsByClassName("btn");
+var card = document.getElementsByClassName("card");
+var icon = document.getElementsByClassName("icon");
+var media = document.getElementsByClassName("animate");
+
+document.body.onload = function () {
+  titles.forEach((title) => {
+    gsap
+      .timeline({
+        default: { ease: "power2.easeOut", duration: 1 },
+        scrollTrigger: {
+          trigger: title,
+          start: "top top+=100%",
+          end: "bottom",
+          toggleActions: "play pause none reverse",
+        },
+      })
+      .fromTo(title, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+  });
+
+  paragraphs.forEach((p) => {
+    gsap
+      .timeline({
+        default: { ease: "power2.easeOut", duration: 1 },
+        scrollTrigger: {
+          trigger: p,
+          start: "top top+=100%",
+          end: "bottom",
+          toggleActions: "play pause none reverse",
+        },
+      })
+      .fromTo(p, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+  });
+
+  buttons.forEach((btn) => {
+    gsap
+      .timeline({
+        default: { ease: "power2.easeOut", duration: 1 },
+        scrollTrigger: {
+          trigger: btn,
+          start: "top top+=100%",
+          end: "bottom",
+          toggleActions: "play pause none reverse",
+        },
+      })
+      .fromTo(btn, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+  });
+
+  medias.forEach((media) => {
+    gsap
+      .timeline({
+        default: { ease: "expo", duration: 3, delay: 2 },
+        scrollTrigger: {
+          trigger: media,
+          start: "top top+=50%",
+          end: "bottom",
+          toggleActions: "play pause none reverse",
+        },
+      })
+      .fromTo(media, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
+  });
+
+  icons.forEach((icon) => {
+    gsap
+      .timeline({
+        default: { ease: "expo", duration: 3, delay: 2 },
+        scrollTrigger: {
+          trigger: icon,
+          start: "top top+=100%",
+          end: "bottom",
+          toggleActions: "play pause none reverse",
+        },
+      })
+      .fromTo(icon, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
+  });
+
+  cards.forEach((card) => {
+    gsap
+      .timeline({
+        default: { ease: "expo", duration: 3, delay: 2 },
+        scrollTrigger: {
+          trigger: card,
+          start: "top top+=80%",
+          end: "bottom",
+          toggleActions: "play pause none reverse",
+        },
+      })
+      .fromTo(card, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
+  });
+};
+
 /*DATEPICKER*/
 
-$(function(){
-    $('#datepicker').datepicker();
-  });    
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
+var toggleCalendar = document.getElementById("openCalendar");
+
+const picker = datepicker("#datepicker", {
+  formatter: (input, date, instance) => {
+    const value = date.toLocaleDateString();
+    input.value = value; // => '1/1/2099'
+  },
+});
+// Toggle the calendar when a button is clicked.
+toggleCalendar.addEventListener("click", (e) => {
+  // THIS!!! Prevent Datepicker's event handler from hiding the calendar.
+  e.stopPropagation();
+
+  // Toggle the calendar.
+  const isHidden = picker.calendarContainer.classList.contains("qs-hidden");
+  picker[isHidden ? "show" : "hide"]();
+});
