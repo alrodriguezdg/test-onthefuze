af7f2c27898018ecd510572d032048952f37f68b
alrodriguezdg <alrodriguezdg@gmail.com>
2023-02-02 13:16:04 -0500

    Indent JS

--- js/functions.js
+++ js/functions.js
@@ -17,111 +17,113 @@ var media = document.getElementsByClassName("animate");
 let mm = gsap.matchMedia();
 
 mm.add("(min-width: 992px)", () => {
+  document.body.onload = function () {
+    titles.forEach((title) => {
+      gsap
+        .timeline({
+          default: { ease: "power2.easeOut", duration: 1 },
+          scrollTrigger: {
+            trigger: title,
+            start: "top top+=100%",
+            end: "bottom",
+            toggleActions: "play pause none reverse",
+          },
+        })
+        .fromTo(title, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+    });
 
-document.body.onload = function () {
-  titles.forEach((title) => {
-    gsap
-      .timeline({
-        default: { ease: "power2.easeOut", duration: 1 },
-        scrollTrigger: {
-          trigger: title,
-          start: "top top+=100%",
-          end: "bottom",
-          toggleActions: "play pause none reverse",
-        },
-      })
-      .fromTo(title, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
-  });
+    paragraphs.forEach((p) => {
+      gsap
+        .timeline({
+          default: { ease: "power2.easeOut", duration: 1 },
+          scrollTrigger: {
+            trigger: p,
+            start: "top top+=100%",
+            end: "bottom",
+            toggleActions: "play pause none reverse",
+          },
+        })
+        .fromTo(p, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+    });
 
-  paragraphs.forEach((p) => {
-    gsap
-      .timeline({
-        default: { ease: "power2.easeOut", duration: 1 },
-        scrollTrigger: {
-          trigger: p,
-          start: "top top+=100%",
-          end: "bottom",
-          toggleActions: "play pause none reverse",
-        },
-      })
-      .fromTo(p, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
-  });
+    buttons.forEach((btn) => {
+      gsap
+        .timeline({
+          default: { ease: "power2.easeOut", duration: 1 },
+          scrollTrigger: {
+            trigger: btn,
+            start: "top top+=100%",
+            end: "bottom",
+            toggleActions: "play pause none reverse",
+          },
+        })
+        .fromTo(btn, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+    });
 
-  buttons.forEach((btn) => {
-    gsap
-      .timeline({
-        default: { ease: "power2.easeOut", duration: 1 },
-        scrollTrigger: {
-          trigger: btn,
-          start: "top top+=100%",
-          end: "bottom",
-          toggleActions: "play pause none reverse",
-        },
-      })
-      .fromTo(btn, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
-  });
+    medias.forEach((media) => {
+      gsap
+        .timeline({
+          default: { ease: "expo", duration: 3, delay: 2 },
+          scrollTrigger: {
+            trigger: media,
+            start: "top top+=50%",
+            end: "bottom",
+            toggleActions: "play pause none reverse",
+          },
+        })
+        .fromTo(media, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
+    });
 
-  medias.forEach((media) => {
-    gsap
-      .timeline({
-        default: { ease: "expo", duration: 3, delay: 2 },
-        scrollTrigger: {
-          trigger: media,
-          start: "top top+=50%",
-          end: "bottom",
-          toggleActions: "play pause none reverse",
-        },
-      })
-      .fromTo(media, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
-  });
+    icons.forEach((icon) => {
+      gsap
+        .timeline({
+          default: { ease: "expo", duration: 3, delay: 2 },
+          scrollTrigger: {
+            trigger: icon,
+            start: "top top+=100%",
+            end: "bottom",
+            toggleActions: "play pause none reverse",
+          },
+        })
+        .fromTo(icon, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
+    });
 
-  icons.forEach((icon) => {
-    gsap
-      .timeline({
-        default: { ease: "expo", duration: 3, delay: 2 },
-        scrollTrigger: {
-          trigger: icon,
-          start: "top top+=100%",
-          end: "bottom",
-          toggleActions: "play pause none reverse",
-        },
-      })
-      .fromTo(icon, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
-  });
-
-  cards.forEach((card) => {
-    gsap
-      .timeline({
-        default: { ease: "expo", duration: 3, delay: 2 },
-        scrollTrigger: {
-          trigger: card,
-          start: "top top+=80%",
-          end: "bottom",
-          toggleActions: "play pause none reverse",
-        },
-      })
-      .fromTo(card, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
-  });
-};
+    cards.forEach((card) => {
+      gsap
+        .timeline({
+          default: { ease: "expo", duration: 3, delay: 2 },
+          scrollTrigger: {
+            trigger: card,
+            start: "top top+=80%",
+            end: "bottom",
+            toggleActions: "play pause none reverse",
+          },
+        })
+        .fromTo(card, { opacity: 0, y: 300 }, { opacity: 1, y: 0 });
+    });
+  };
 });
 
 /*FIXED MENU*/
 
-let scrollHeader = window.scrollY
-  const header = document.querySelector("header")
-  const headerHeight = header.offsetHeight
+let scrollHeader = window.scrollY;
+const header = document.querySelector("header");
+const headerHeight = header.offsetHeight;
 
-  const addClass = () => header.classList.add("fixed")
-  const removeClass = () => header.classList.remove("fixed")
+const addClass = () => header.classList.add("fixed");
+const removeClass = () => header.classList.remove("fixed");
 
-  window.addEventListener('scroll', function() { 
-    scrollHeader = window.scrollY;
+window.addEventListener("scroll", function () {
+  scrollHeader = window.scrollY;
 
-    if (scrollHeader >= headerHeight) { addClass() }
-    else { removeClass() }
+  if (scrollHeader >= headerHeight) {
+    addClass();
+  } else {
+    removeClass();
+  }
 
-    console.log(scrollHeader)
-  })
+  console.log(scrollHeader);
+});
 
 //DATEPICKER
 
